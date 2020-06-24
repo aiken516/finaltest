b20b0b34e22f3b50286733332c496360124db862
aiken516 <jimmy000516@naver.com>
2020-06-24 10:05:19 +0900

    커밋2

---
+++ finalexam02.py
@@ -0,0 +1,26 @@
+class Point:
+    __x = 0
+    __y = 0
+
+    def __init__(self, xn=0, yn=0):
+        self.__x = xn
+        self.__y = yn
+
+    def getX(self):
+        return self.__x
+    
+    def getY(self):
+        return self.__y
+
+    def distance(point1, point2):
+        return ((point1.getX() - point2.getX())**2 + (point1.getY() - point2.getY())**2)**(1/2)
+
+    def __add__(point1, point2):
+        return Point(point1.getX() + point2.getX(), point1.getY() + point2.getY())
+
+
+p1 = Point(1,1)
+p2 = Point(2,2)
+print(Point.distance(p1,p2))
+p3 = p1 + p2
+print("p1:{0:d}, p2:{1:d}".format(p3.getX(), p3.getY()))
